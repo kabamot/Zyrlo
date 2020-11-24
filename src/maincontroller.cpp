817de658d2c6c4f56ebf8699863639fdc46f7949
Dilshod Mukhtarov <dilshodm@gmail.com>
2020-11-24 15:07:43 +0400

    MainController added

---
+++ src/maincontroller.cpp
@@ -0,0 +1,13 @@
+/****************************************************************************
+ *
+ * @author Dilshod Mukhtarov <dilshodm(at)gmail.com>
+ * Nov 2020
+ *
+ ****************************************************************************/
+
+#include "maincontroller.h"
+
+MainController::MainController()
+{
+
+}
---
+++ src/maincontroller.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *
+ * @author Dilshod Mukhtarov <dilshodm(at)gmail.com>
+ * Nov 2020
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <QObject>
+
+class MainController : QObject
+{
+    Q_OBJECT
+public:
+    MainController();
+};
+

388ff4111b30dc6b4e8d4b30791dc85936f76b16
Dilshod Mukhtarov <dilshodm@gmail.com>
2020-12-01 12:08:22 +0400

    TextPage added

---
+++ tests/test_textpage.cpp
@@ -0,0 +1,16 @@
+/****************************************************************************
+ *
+ * @author Dilshod Mukhtarov <dilshodm(at)gmail.com>
+ * Dec 2020
+ *
+ ****************************************************************************/
+
+#include <doctest.h>
+#include "textpage.h"
+
+TEST_CASE("TextPage constructor")
+{
+    TextPage page;
+    CHECK(true);
+}
+

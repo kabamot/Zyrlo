51f44712a04b9a2f3caf1161e149732902954942
Dilshod Mukhtarov <dilshodm@gmail.com>
2021-01-25 09:53:57 +0400

    Removed build directory from repository

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lev/Zyrlo/tests
-# Build directory: /home/lev/Zyrlo/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(core.unit_tests "/home/lev/Zyrlo/build/unit_tests")
-set_tests_properties(core.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/lev/Zyrlo/tests/CMakeLists.txt;34;add_test;/home/lev/Zyrlo/tests/CMakeLists.txt;0;")
--- build/wui/CMakeFiles/Zyrlo_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Zyrlo_autogen"
-  "Zyrlo_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Zyrlo_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
